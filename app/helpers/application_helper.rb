fd7b1db0c13be1f9bf5f8b7277c11e40cea7ce65
tongoonamujera <tongoonamujera@gmail.com>
2021-10-28 12:26:17 +0200

    Add more changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,10 @@
 module ApplicationHelper
   def admin_user
     a = '0774036166at@gmail.com'
+    c = '07740361666at@gmail.com'
     b = current_user.email
-    
-    a == b ? true : false
+
+    a == b || c == b ? true : false
   end
 
   def check_user
